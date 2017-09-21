925d1069149c39764b689b9433b44689d59622c1
akshayshetty <ashetty2@illinois.edu>
2017-09-21 14:47:54 -0700

    Added files for A100-2 and fixed a typo

--- launch_scripts/matrixvision.sh
+++ launch_scripts/matrixvision.sh
@@ -4,7 +4,8 @@ cd ~/Visual-SLAM/mvbluefox_ws
 source devel/setup.bash
 
 case $HOSTNAME in
-	(safe50-UDOO-x86) export ROS_IP=192.168.0.222; roslaunch bluefox2 bluefox2.launch device:=25002090;;
+	(safe50-UDOO-x86) export ROS_IP=192.168.0.220; roslaunch bluefox2 bluefox2.launch device:=25002315;;	
+	(safe50-nuc1) export ROS_IP=192.168.0.222; roslaunch bluefox2 bluefox2.launch device:=25002090;;
 	(safe50-JOULE-x86) export ROS_IP=192.168.0.111; roslaunch bluefox2 bluefox2.launch device:=25002112;;
 	(*) echo "Computer not recognized!";;
 esac
--- launch_scripts/mavros_start.sh
+++ launch_scripts/mavros_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
 case $HOSTNAME in
-	(safe50-UDOO-x86) export ROS_IP=192.168.0.222;roslaunch mavros px4.launch;;
+	(safe50-UDOO-x86) export ROS_IP=192.168.0.220;roslaunch mavros px4.launch;;
 	(*) echo "Computer not recognized!";;
 esac
